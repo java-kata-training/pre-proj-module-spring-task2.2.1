7edcfd220c508c5938778f652c890b1d55156d07
dimolke <dimolke@gmail.com>
2026-05-14 18:03:54 +0300

    task 2.2.1 completed - 1st try

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,13 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.transaction.Transactional;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,11 +17,30 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+       CarService carService = context.getBean(CarService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+//       Car car1 = new Car(1L, "Lamborghini", 570);
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+//              new Car(1L, "Lamborghini", 570)));
+//
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+//              new Car(2L, "Bugatti", 110)));
+//
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
+//              new Car(3L, "Mercedes-Maybach", 600)));
+//
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
+//              new Car(4L, "Ferrari", 90)));
+//
+//      userService.add(new User("User5", "Lastname4", "user4@mail.ru",
+//              new Car(5L, "Aurus", 5)));
+
+       userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Lamborghini", 570)));
+       userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Bugatti", 110)));
+       userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Mercedes-Maybach", 600)));
+       userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Aurus", 5)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +48,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+          System.out.println("Car = " + user.getCar());
          System.out.println();
       }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,12 +19,18 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   //cascade = CascadeType.ALL в аннотации @OneToOne автоматически сохраняет машину при сохранении пользователя
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private Car car;
+
    public User() {}
    
-   public User(String firstName, String lastName, String email) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
@@ -58,4 +64,8 @@ public class User {
    public void setEmail(String email) {
       this.email = email;
    }
+
+    public void setCar(Car car) {this.car = car;}
+
+    public Car getCar() {return car;}
 }
